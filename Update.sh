52c310439f22d4127cbd548821af9ca7f6c5e63f
Darken-Pasiya <91953654+Darken-Pasiya@users.noreply.github.com>
2021-10-19 12:52:36 +0530

    Create Update.sh

---
+++ Update.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+#
+# Termux Banner
+# Banner by Darken Pasiya
+# _____Do Not Copy Or Modify_____
+#
+#
+
+clear
+figlet Updating | lolcat
+sleep 1
+cd
+git clone https://github.com/Darken-Pasiya/D-Banner
+clear
+cd D-Banner
+clear
+sleep 2
+toilet -f mono12 -F border Finish | lolcat
+echo 
+echo 
+echo 
